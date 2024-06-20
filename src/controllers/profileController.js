fc251d3e8f1d5e21680d4c2b9a69b63ea15cda0f
Anas4rif <112615637+Anas4rif@users.noreply.github.com>
2024-06-20 21:04:29 +0700

    repair profile
    SECRET_KEY

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -2,18 +2,22 @@ const db = require('../config/mysqlConfig'); // Pastikan Anda sudah mengatur kon
 
 exports.getProfile = async (req, res) => {
     try {
-        const userId = req.user.id; // Asumsikan Anda memiliki middleware otentikasi yang menetapkan req.user
+        const userId = req.user.id;
+        console.log('User ID from token:', userId);
 
         const [rows] = await db.query('SELECT name, age, email, gender FROM users WHERE id = ?', [userId]);
 
         if (rows.length === 0) {
+            console.log('User not found');
             return res.status(404).json({ message: 'User not found' });
         }
 
         const userProfile = rows[0];
+        console.log('User profile:', userProfile);
 
         res.status(200).json(userProfile);
     } catch (err) {
+        console.log('Error fetching user profile:', err.message);
         res.status(500).json({ message: err.message });
     }
 };
